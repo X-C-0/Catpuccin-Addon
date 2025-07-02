9a51908a5340bfa038fbd497dea1b5275037f162
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2025-07-02 17:34:31 +0200

    Change @Redirect to safer @WrapWithCondition

--- src/main/java/me/pindour/catpuccin/mixin/meteorclient/WHeaderMixin.java
+++ src/main/java/me/pindour/catpuccin/mixin/meteorclient/WHeaderMixin.java
@@ -1,30 +1,30 @@
 package me.pindour.catpuccin.mixin.meteorclient;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import me.pindour.catpuccin.gui.themes.catpuccin.CatpuccinGuiTheme;
 import meteordevelopment.meteorclient.gui.widgets.containers.WWindow;
 import meteordevelopment.meteorclient.gui.widgets.pressable.WTriangle;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "meteordevelopment.meteorclient.gui.widgets.containers.WWindow$WHeader", remap = false)
 public abstract class WHeaderMixin {
-
-    @Shadow @Final
+    @Shadow
+    @Final
     WWindow this$0;
 
-    @Redirect(method = "render", at = @At(
-                    value = "FIELD",
-                    target = "Lmeteordevelopment/meteorclient/gui/widgets/pressable/WTriangle;rotation:D",
-                    opcode = Opcodes.PUTFIELD))
-
-    private void onRender(WTriangle triangle, double value) {
-        if (!(this$0.theme instanceof CatpuccinGuiTheme)) {
-            // Ignore triangle rotation if we are using our theme (idiot null triangul)
-            triangle.rotation = value;
-        }
+    @WrapWithCondition(
+        method = "render",
+        at = @At(
+            value = "FIELD",
+            target = "Lmeteordevelopment/meteorclient/gui/widgets/pressable/WTriangle;rotation:D",
+            opcode = org.objectweb.asm.Opcodes.PUTFIELD
+        )
+    )
+    private boolean shouldSetRotation(WTriangle triangle, double value) {
+        // Only allow rotation assignment if we're not using our theme
+        return !(this$0.theme instanceof CatpuccinGuiTheme);
     }
 }
