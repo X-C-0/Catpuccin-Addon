d3b14ee79fb0725998a06b53f852b6d4d64f39d7
XC0 <102973604+X-C-0@users.noreply.github.com>
2025-07-05 18:39:24 +0200

    Made bottom bar in module screen sticky

--- src/main/java/me/pindour/catpuccin/gui/themes/catpuccin/widgets/WCatpuccinView.java
+++ src/main/java/me/pindour/catpuccin/gui/themes/catpuccin/widgets/WCatpuccinView.java
@@ -3,8 +3,14 @@ package me.pindour.catpuccin.gui.themes.catpuccin.widgets;
 import me.pindour.catpuccin.gui.themes.catpuccin.CatpuccinWidget;
 import meteordevelopment.meteorclient.gui.renderer.GuiRenderer;
 import meteordevelopment.meteorclient.gui.widgets.containers.WView;
+import meteordevelopment.meteorclient.utils.Utils;
 
 public class WCatpuccinView extends WView implements CatpuccinWidget {
+
+    @Override
+    public void init() {
+        maxHeight = Utils.getWindowHeight() - theme.scale(150);
+    }
 
     @Override
     protected void onRender(GuiRenderer renderer, double mouseX, double mouseY, double delta) {
