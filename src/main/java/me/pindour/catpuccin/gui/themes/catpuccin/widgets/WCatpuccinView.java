959eedbdf25ecafb6c39c0c3186a7797cf0fcc62
XC0 <102973604+x-c-0@users.noreply.github.com>
2025-10-27 23:48:35 +0100

    round scrollbar

--- src/main/java/me/pindour/catpuccin/gui/themes/catpuccin/widgets/WCatpuccinView.java
+++ src/main/java/me/pindour/catpuccin/gui/themes/catpuccin/widgets/WCatpuccinView.java
@@ -1,5 +1,6 @@
 package me.pindour.catpuccin.gui.themes.catpuccin.widgets;
 
+import me.pindour.catpuccin.gui.renderer.CornerStyle;
 import me.pindour.catpuccin.gui.themes.catpuccin.CatpuccinWidget;
 import meteordevelopment.meteorclient.gui.renderer.GuiRenderer;
 import meteordevelopment.meteorclient.gui.widgets.containers.WView;
@@ -15,12 +16,14 @@ public class WCatpuccinView extends WView implements CatpuccinWidget {
     @Override
     protected void onRender(GuiRenderer renderer, double mouseX, double mouseY, double delta) {
         if (canScroll && hasScrollBar) {
-            renderer.quad(
+            catpuccinRenderer().roundedRect(
                     handleX(),
                     handleY(),
                     handleWidth(),
                     handleHeight(),
-                    theme().scrollbarColor.get(handlePressed, handleMouseOver)
+                    6,
+                    theme().scrollbarColor.get(handlePressed, handleMouseOver),
+                    CornerStyle.ALL
             );
         }
     }
