16efd1327d5a99c7d8479c1e0c551972f85cdac6
XC0 <102973604+X-C-0@users.noreply.github.com>
2026-02-02 00:48:12 +0100

    fix: Add margin to tab bar only when Catppuccin is active

--- src/main/java/me/pindour/catppuccin/mixin/meteorclient/TabMixin.java
+++ src/main/java/me/pindour/catppuccin/mixin/meteorclient/TabMixin.java
@@ -1,25 +1,33 @@
 package me.pindour.catppuccin.mixin.meteorclient;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import me.pindour.catppuccin.gui.themes.catppuccin.CatppuccinGuiTheme;
+import meteordevelopment.meteorclient.gui.GuiThemes;
 import meteordevelopment.meteorclient.gui.tabs.Tab;
 import meteordevelopment.meteorclient.gui.tabs.TabScreen;
 import meteordevelopment.meteorclient.gui.utils.Cell;
 import meteordevelopment.meteorclient.gui.widgets.WWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = Tab.class, remap = false)
 public abstract class TabMixin {
 
-    @Redirect(
+    @WrapOperation(
         method = "openScreen",
         at = @At(
             value = "INVOKE",
             target = "Lmeteordevelopment/meteorclient/gui/tabs/TabScreen;addDirect(Lmeteordevelopment/meteorclient/gui/widgets/WWidget;)Lmeteordevelopment/meteorclient/gui/utils/Cell;"
         )
     )
-    public Cell<?> catppuccin$addDirect(TabScreen screen, WWidget widget) {
-        // Add a small margin at the top
-        return screen.addDirect(widget).marginTop(10).top().centerX();
+    private Cell<?> catppuccin$addTopBarMargin(TabScreen screen, WWidget widget, Operation<Cell<?>> original) {
+        Cell<?> cell = original.call(screen, widget);
+
+        // Add a small margin at the top, but only when our theme is active
+        if (GuiThemes.get() instanceof CatppuccinGuiTheme)
+            cell.marginTop(10);
+
+        return cell;
     }
 }
