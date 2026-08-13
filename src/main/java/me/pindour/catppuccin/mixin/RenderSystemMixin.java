9a8727a9df15427808bf6a18b6ed063fe72f4950
XC0 <102973604+x-c-0@users.noreply.github.com>
2026-08-13 15:59:36 +0200

    fix: Renderer not "flipping" frame (oopsies)

--- src/main/java/me/pindour/catppuccin/mixin/RenderSystemMixin.java
+++ src/main/java/me/pindour/catppuccin/mixin/RenderSystemMixin.java
@@ -3,7 +3,7 @@ package me.pindour.catppuccin.mixin;
 import com.mojang.blaze3d.systems.RenderSystem;
 import org.spongepowered.asm.mixin.Mixin;
 
-//? if <=26.1 {
+//? if <=26.1.2 {
 /*import me.pindour.catppuccin.renderer.CatppuccinRenderer;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSystem.class)
 public abstract class RenderSystemMixin {
-    //? if <=26.1 {
+    //? if <=26.1.2 {
     /*@Inject(method = "flipFrame", at = @At("TAIL"))
     private static void catppuccin$flipFrame(CallbackInfo info) {
         CatppuccinRenderer.get().flipFrame();
