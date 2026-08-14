c1a4359b06d430c077e7b3686266a1363d89f711
XC0 <102973604+x-c-0@users.noreply.github.com>
2026-08-14 15:57:10 +0200

    fix: Made favorite icon a bit bigger

--- src/main/java/me/pindour/catppuccin/gui/themes/catppuccin/widgets/pressable/WCatppuccinFavorite.java
+++ src/main/java/me/pindour/catppuccin/gui/themes/catppuccin/widgets/pressable/WCatppuccinFavorite.java
@@ -7,20 +7,34 @@ import meteordevelopment.meteorclient.gui.widgets.pressable.WFavorite;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 
 public class WCatppuccinFavorite extends WFavorite implements CatppuccinWidget {
+    double size;
+
     public WCatppuccinFavorite(boolean checked) {
         super(checked);
     }
 
     @Override
+    public void init() {
+        size = theme.textHeight() * 1.15f;
+    }
+
+    @Override
+    protected void onCalculateSize() {
+        double pad = pad();
+
+        width = pad + size + pad;
+        height = pad + size + pad;
+    }
+
+    @Override
     protected void onRender(GuiRenderer renderer, double mouseX, double mouseY, double delta) {
         double pad = pad();
-        double s = theme.textHeight();
 
         renderer.quad(
                 x + pad,
                 y + pad,
-                s,
-                s,
+                size,
+                size,
                 checked ? CatppuccinBuiltinIcons.BOOKMARK_YES.texture() : CatppuccinBuiltinIcons.BOOKMARK_NO.texture(),
                 getColor()
         );
